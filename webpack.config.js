772fd19afce0bd7e2a9062f13ce2072f2804f422
masami.shimbuichi <kei11nishikori16@gmail.com>
2019-08-25 13:51:06 +0900

    docsへ公開ページを変更

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const targetTypes = { ejs: 'html', js: 'js' }
+const cssTarget = { styl: 'css' }
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const publishDir = 'docs'
+
+const app = {
+  entry: 'src/index.ejs',
+  output: {
+    filename: '[name]',
+    path: `${__dirname}/${publishDir}`
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.ejs$/,
+      use: [
+        'html-loader',
+        'ejs-html-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/index.ejs'
+    }),
+    new CopyWebpackPlugin(
+      [{ from: `${__dirname}/src` }],
+      { ignore: Object.keys(targetTypes).map((ext) => `*.${ext}`) }),
+    new CopyWebpackPlugin(
+      [{ from: `${__dirname}/css/styl/**/*`, to: `${__dirname}/${publishDir}/css/` }],
+      { context: 'css/' },
+      { ignore: ['*.styl'] }),
+    new CopyWebpackPlugin(
+      [{ from: `${__dirname}/js/**/*`, to: `${__dirname}/${publishDir}/js/` }],
+      { context: 'js/' })
+  ]
+}
+
+module.exports = app
